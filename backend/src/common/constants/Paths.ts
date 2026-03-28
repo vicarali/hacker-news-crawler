4080962b2b56ca5ef36e5fc0148ceb85ebac0f4a
Vicarali <vicarali@gmail.com>
2026-03-28 20:00:55 +0100

    chore: remove old User routes

--- backend/src/common/constants/Paths.ts
+++ backend/src/common/constants/Paths.ts
@@ -1,12 +1,5 @@
 const Paths = {
   _: '/api',
-  Users: {
-    _: '/users',
-    Get: '/all',
-    Add: '/add',
-    Update: '/update',
-    Delete: '/delete/:id',
-  },
   HackerNews: {
     _: '/hacker-news',
     Get: '/all',
